2fa194230bcfbd939c6f8a6448a4b880343b998e
davutbilgicnl <davutbilgic.nl@gmail.com>
2023-11-08 09:32:13 +0100

    ConditionalContent&ASuboptimalWayOf UpdatingState

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,13 +4,13 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required></input>;
+    playerName = <input type="text" required value={name}></input>;
   }
 
   return (
@@ -18,7 +18,7 @@ export default function Player({ name, symbol }) {
       <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
-        <button onClick={handleEditClick}>Edit</button>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
       </span>
     </li>
   );
